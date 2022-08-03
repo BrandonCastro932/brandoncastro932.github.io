5c8ae263451baae017ded5cd854c3ef525002064
Brandon Castro <brandoncastro12331@gmail.com>
2022-08-03 16:17:57 -0600

    First commit

---
+++ src/components/Greetings.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class Greetings extends Component{
+    constructor(){
+        super();
+        this.state= {
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h2>Mi componente de react!</h2>
+            </div>
+
+        )}
+}
+export default Greetings();
